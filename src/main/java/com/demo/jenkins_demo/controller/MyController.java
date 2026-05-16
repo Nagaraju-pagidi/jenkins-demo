db05227ce20e184c252446b6e8ca51414029c29b
Nagaraju Pagigi <pnagaraju3001@gmail.com>
2026-05-16 11:22:42 +0530

    added controlelr

---
+++ src/main/java/com/demo/jenkins_demo/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.demo.jenkins_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "WELCOME TO SPRING BOOT WITH JENKINS CI pipeline";
+	}
+}
